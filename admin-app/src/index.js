9e80acc7a002f8c3dabdd591cc5bc143a4b57418
Sỷ Anh Nguyễn Bảo <nbsyanh1998@gmail.com>
2019-11-20 14:18:53 +0700

    init frontend project

--- admin-app/src/index.js
+++ admin-app/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 import './styles/index.css';
 import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store= createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
